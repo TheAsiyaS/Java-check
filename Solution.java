d93ad643b9bdae2bc6464bd3432984ac4ae74bd7
TheAsiyaS <theasiya.s@gmail.com>
2025-08-09 23:00:45 +0530

    ref: syntax change

--- Solution.java
+++ Solution.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class Solution {
    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int num1 = scan.nextInt();
-       int num2 = scan.nextInt();
-       int num3 = scan.nextInt();
-        System.out.println(num1);
-       System.out.println(num2);
-       System.out.println(num3);
+        try (Scanner scan = new Scanner(System.in)) {
+            int num1 = scan.nextInt();
+      int num2 = scan.nextInt();
+      int num3 = scan.nextInt();
+            System.out.println(num1);
+      System.out.println(num2);
+      System.out.println(num3);
+        }
     }
 
     
