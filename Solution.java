4190f985877eff00be222d455784eeba0f3c5f9e
TheAsiyaS <theasiya.s@gmail.com>
2025-08-09 22:58:22 +0530

    java input

---
+++ Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+   public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+       int num2 = scan.nextInt();
+       int num3 = scan.nextInt();
+        System.out.println(num1);
+       System.out.println(num2);
+       System.out.println(num3);
+    }
+
+    
+}
+
+ 
